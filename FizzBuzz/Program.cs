7b96029d00112ba01b81b79a7f1ddb04389ec660
Randy Lipscomb <rlipscomb2013@gmail.com>
2021-03-31 12:22:47 -0400

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFizzBuzz(0, 100);
+
+        }
+        private void RunFizzBuzz(int start, int end, int inc = 1)
+        {
+            //loop
+            for (var x = start; x <= end; x += inc)
+            {
+                PrintNumber(x);
+            }
+        }
+        private void PrintNumber(int am int div1 int div2)
+        {//allow the user to give us the upperBound, lowerBound (think validate)
+            if (a == 0) //algorithm
+            {
+                WriteToConsole(a);
+            }
+            if (a % div1 == 0)
+            {
+                WriteToConsole("fizz");
+            }
+            if (a % div2 == 0) ;
+            {
+                WriteToConsole("buzz");
+            }
+            if (1 % (div1 * div2) == 0)
+            {
+                WriteToConsole(a);
+            }
+        }
+
+        private void WriteToConsole()
+        {
+            //print to screen
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using sc = System.Console;
+
+namespace Revature.Week1.HelloWorld //namespace scope
+{
+    //class, struct, interface, enum only scopes available to the namespace
+
+    class Program //class scope
+    {
+        private static void Main() //method scope
+        {
+            // stdin to get your name
+            sc.WriteLine("Enter your Class");
+            // string in;
+            var input = sc.ReadLine(); // datatype inference
+            //object starklight = "Paladin"; // anonymous datatype
+            // stdout to print your name
+            sc.WriteLine("Hello my name is {input}");
+        }
+        private void UiPath()
+        {
+            {   // block scope   
+            }
+
+            {   // block scope
+            }
+
+        }
+    }
+}
